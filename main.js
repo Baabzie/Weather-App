e0845dc5310f2ee801c41689d29201eca961be80
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2023-03-06 16:56:10 +0100

    nav function created

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-// const weekViewDiv = document.getElementById("weekViewDiv");
+const onVisit = async () => {
+    navigator.geolocation.getCurrentPosition(success, error);
+}
 
-// weekViewDiv.innerHTML = "";
+function success(pos) {
+    const crd = pos.coords;
+  
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    console.log(`We can't find your location!`);
+}
+
+onVisit();
